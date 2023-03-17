4bcbf16ab7c9d9fb218979468d8053c96f20586e
jahill <jahill@groupon.com>
2023-03-17 21:11:01 +0000

    feat: added initial draft of retry fetch

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,66 @@
 /* -----------------------------------
  *
- * retryFetch
+ * IOptions
  *
  * -------------------------------- */
 
-async function retryFetch() {
-  // todo
+interface IOptions extends Request {
+  retries?: number;
+  timeout: number;
+}
+
+/* -----------------------------------
+ *
+ * Fetch
+ *
+ * -------------------------------- */
+
+async function retryFetch(
+  requestUrl: string,
+  { retries = 3, timeout = 30e3, ...options }: Partial<IOptions>
+): Promise<Response> {
+  let error: Error = new Error();
+  const meta = { requestUrl, retries, options, timeout: false };
+  const control = new AbortController();
+  let attempt = 1;
+
+  while (retries > 0) {
+    const onTimeout = setTimeout(() => (meta.timeout = true) && control.abort(), timeout);
+
+    try {
+      const result: Response = await fetch(requestUrl, {
+        ...options,
+        signal: control.signal,
+      });
+
+      clearTimeout(onTimeout);
+
+      if (result.ok) {
+        return result;
+      }
+    } catch (reason) {
+      error = reason as Error;
+    }
+
+    await sleep(2 ** attempt * 10e3);
+
+    retries -= 1;
+    attempt += 1;
+  }
+
+  error = Object.assign(error, { meta });
+
+  throw error;
+}
+
+/* -----------------------------------
+ *
+ * Sleep
+ *
+ * -------------------------------- */
+
+async function sleep(seconds = 1): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1e3));
 }
 
 /* -----------------------------------
